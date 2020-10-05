6e2dab7d46ece584ff87f74d0abf4daa8fefac06
jayharville <jayharville@aol.com>
2020-10-04 19:55:24 -0700

    Completed mvp

---
+++ src/components/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const TodoForm = props => {
+  return (
+    <form>
+      <input
+        onChange={props.handleTodoChange}
+        type="text"
+        name="todo"
+        value={props.value}
+        placeholder="Add Task"
+      />
+      <button onClick={props.handleAddTodo}>Add Task</button>
+      <button onClick={props.handleClearTodos}>Clear Completed</button>
+    </form>
+  );
+};
+
+export default TodoForm;
